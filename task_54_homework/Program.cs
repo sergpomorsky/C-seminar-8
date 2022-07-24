84662ba48835c570bf27a0574d229069a8849925
deardaddy <sergpomorsky@gmail.com>
2022-07-24 12:06:15 +0300

    add task 54 homework

---
+++ task_54_homework/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива
+int[,] massive = new int[3, 4];
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+      for (int j = 0; j < massive.GetLength(1); j++)
+      {
+            massive[i, j] = new Random().Next(1, 10);
+            Console.Write(massive[i, j] + " ");
+      }
+      Console.WriteLine();
+}
+Console.WriteLine();
+void ArraySort(int[,] Array)
+      {
+            for (int i = 0; i < Array.GetLength(1); i++)
+            {
+                for (int j = 0; j < Array.GetLength(1) - 1; j++)
+                  {
+                    for (int k = 0; k < Array.GetLength(1)-1; k++)
+                    {
+                        if(Array[k,j]>Array[k,j+1])
+                        {
+                              int temp=Array[k,j+1];
+                              Array[k,j+1]=Array[k,j];
+                              Array[k,j]=temp;
+                              temp=0;
+                        }
+                    }
+                    
+                       
+                  }
+                }
+      }
+      
+ArraySort(massive);
+
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+      for (int j = 0; j < massive.GetLength(1); j++)
+      {
+            Console.Write(massive[i, j] + " ");
+      }
+      Console.WriteLine();
+}
+Console.WriteLine();
